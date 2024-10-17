609e5a18fe9c39fb7f367c96c023066b8bc675cd
Bryan Fichera <bfichera@anl.gov>
2024-10-17 15:45:34 -0500

    Fix typo in open

--- bulletproof_open/open.py
+++ bulletproof_open/open.py
@@ -4,12 +4,13 @@ from uuid import uuid4
 
 
 logger = logging.getLogger(__file__)
+open_ = open
 
 
 def open(file, *args, **kwargs):
     path = Path(file)
     if path.exists():
-        newpath = path.parent / (path.name+uuid4().hex)
+        newpath = path.parent / (path.name+'.'+uuid4().hex)
         logger.warning(f'Path {path} exists! Creating new filepath {newpath}.')
         return open(newpath, *args, **kwargs)
     if not path.parent.exists():
@@ -18,4 +19,4 @@ def open(file, *args, **kwargs):
             f'Directory path {path.parent} does not exist!'
             f' Creating new directory (including parents) {path.parent}.'
         )
-    return open(path, *args, **kwargs)
+    return open_(path, *args, **kwargs)
