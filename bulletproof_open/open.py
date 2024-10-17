d3f2555e2a531655d41ec190df4dc4c3f294d8fe
Bryan Fichera <bfichera@anl.gov>
2024-10-17 15:19:08 -0500

    Initial commit

---
+++ bulletproof_open/open.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import logging
+from uuid import uuid4
+
+
+logger = logging.getLogger(__file__)
+
+
+def open(file, *args, **kwargs):
+    path = Path(file)
+    if path.exists():
+        newpath = path.parent / (path.name+uuid4().hex)
+        logger.warning(f'Path {path} exists! Creating new filepath {newpath}.')
+        return open(newpath, *args, **kwargs)
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True, exist_ok=False)
+        logger.warning(
+            f'Directory path {path.parent} does not exist!'
+            f' Creating new directory (including parents) {path.parent}.'
+        )
+    return open(path, *args, **kwargs)
