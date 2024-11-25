f57e2708c58e415bb27c274317b57ef29b12e0f2
Bryan Fichera <bfichera@anl.gov>
2024-11-25 12:12:07 -0600

    Don't overwrite open if 'w' not in mode

--- bulletproof_open/open.py
+++ bulletproof_open/open.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import logging
+import inspect
 from uuid import uuid4
 
 
@@ -7,16 +8,28 @@ logger = logging.getLogger(__file__)
 open_ = open
 
 
-def open(file, *args, **kwargs):
-    path = Path(file)
-    if path.exists():
-        newpath = path.parent / (path.name+'.'+uuid4().hex)
-        logger.warning(f'Path {path} exists! Creating new filepath {newpath}.')
-        return open(newpath, *args, **kwargs)
-    if not path.parent.exists():
-        path.parent.mkdir(parents=True, exist_ok=True)
-        logger.warning(
-            f'Directory path {path.parent} does not exist!'
-            f' Creating new directory (including parents) {path.parent}.'
-        )
-    return open_(path, *args, **kwargs)
+def open(file, mode='r', buffering=-1, encoding=None,
+         errors=None, newline=None, closefd=True, opener=None):
+
+    frame = inspect.currentframe()
+    args, _, _, values = inspect.getargvalues(frame)
+    kwargs = {a: v for a, v in zip(args, values)}
+
+    if 'w' in mode:
+        path = Path(file)
+        kwargs['file'] = path
+        if path.exists():
+            newpath = path.parent / (path.name+'.'+uuid4().hex)
+            logger.warning(
+                f'Path {path} exists! Creating new filepath {newpath}.',
+            )
+            kwargs['file'] = newpath
+            return open(**kwargs)
+        if not path.parent.exists():
+            path.parent.mkdir(parents=True, exist_ok=True)
+            logger.warning(
+                f'Directory path {path.parent} does not exist!'
+                f' Creating new directory (including parents) {path.parent}.'
+            )
+        return open_(**kwargs)
+    return open_(**kwargs)
